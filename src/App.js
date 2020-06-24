1f9d1e7c9419a49991f4ecab34688c2af6fd9080
Muhammad Umer <mumer1297@gmail.com>
2020-06-24 11:45:25 +0500

    fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let data = { title: "Title" };
+  const [todo, setTodo] = useState(data);
+  const [fetching, setFetching] = useState(false);
+  const [isData, setData] = useState(false);
+  useEffect(() => {
+    async function fetchData() {
+      setFetching(true);
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      let data2 = await response.json();
+      setTodo(data2);
+      setFetching(false);
+      console.log("data", data);
+    }
+    fetchData();
+  }, [isData]);
+
+  if (fetching) {
+    return <div>Loading</div>;
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{todo.title};</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Made by
+          <a
+            className="App-link"
+            href="https://github.com/iMuhammadUmer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Muhammad Umer
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
